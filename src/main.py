2ad2cf66393ffad07efd34d82ed8c6883202cc28
beard <joshuabeard92@gmail.com>
2019-05-05 09:55:29 -0400

    wip

--- src/main.py
+++ src/main.py
@@ -1,4 +1,20 @@
-from torchvision import models
+from os.path import realpath
 
-if __name__ == "__main__":
-    model = models.squeezenet1_0(pretrained=True)
+# 2019-05-05: Removed from pip-installable version for some reason???
+#from torchvision.datasets import ImageNet
+
+from models.squeezenet import TransparentSqueezeNet
+
+# Higher verbosity means more command-line output
+VERBOSITY = 1
+
+# Download models to ~/.cache (implicitly)
+# Download data to ~/data (explicitly)
+data_dir = realpath('~/data')
+if VERBOSITY > 0:
+    print("Data location: {}".format(data_dir))
+
+images_fpath = 'test/images'
+
+model = TransparentSqueezeNet(pretrained=True)
+#dataset = ImageNet(root=data_dir, split='val', download=True)
