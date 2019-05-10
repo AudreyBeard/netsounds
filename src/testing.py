81f0fc66eb59b3e8e96134f9427f5c9434f9e44c
Josh Beard <joshuabeard92@gmail.com>
2019-05-10 15:38:47 -0400

    playing with sampling rate

--- src/testing.py
+++ src/testing.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 from plottingtools import plot
+import ubelt as ub
 
 import utils
 
@@ -13,11 +14,15 @@ def interp(signal, newlen):
     return sig
 
 
-label = 'mixing-bowl'
+label = ub.argval('label', default='wine-bottle')
+method = ub.argval('method', default='concat')
+sampling_rate = int(ub.argval('sampling_rate', default=44100//4))
+tuned = ub.argflag('--tuned')
+show = ub.argflag('--show')
 dpath = 'test/activations'
-method = 'sum'
-sampling_rate = 44100//4
 filenames = [os.path.join(dpath, l) for l in os.listdir(dpath) if label in l]
+if len(filenames) == 0:
+    raise ValueError('No files found of {} at {}'.format(label, dpath))
 
 raw = [utils.activations_to_audio(utils.load_activations(fn), method)
        for fn in filenames]
@@ -31,13 +36,21 @@ interpped[0, :] = raw[0]
 for i in range(1, len(raw)):
     interpped[i] = interp(raw[i], interpped.shape[1])
 
-summed = interpped.sum(0)
+if tuned:
+    summed = interpped.prod(0)
+else:
+    summed = interpped.sum(0)
 summed = 2 * (summed - summed.min()) / (summed.max() - summed.min()) - 1
 
 title = 'All {} activations {}'.format(method, label)
-if not os.path.isfile('graph_{}.png'.format('_'.join(title.split()))):
+if show and not os.path.isfile('graph_{}.png'.format('_'.join(title.split()))):
     print('saving graph...')
     plot.Lines(y=summed,
                title=title).save()
-
-utils.save_as_wav(summed, 'all-{}_{}'.format(method, label), sampling_rate)
+audio_title = 'all-{}_{}_{}_{}-Hz'.format(
+    method,
+    'tuned' if tuned else '',
+    label,
+    sampling_rate
+)
+utils.save_as_wav(summed, audio_title, sampling_rate)
