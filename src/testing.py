2a6f886ca040869e5dbd2767a2d512475974eca5
beard <joshuabeard92@gmail.com>
2019-08-16 21:21:57 -0400

    eliminate reliance on ubelt, minor changes for clarity and such

--- src/testing.py
+++ src/testing.py
@@ -1,10 +1,14 @@
 import os
+import argparse
 
 import numpy as np
 from plottingtools import plot
-import ubelt as ub
 
 import utils
+
+# TODO:
+# [x] remove reliance on ubelt
+# [ ] shift reliance from numpy towards torch for speed
 
 
 def interp(signal, newlen):
@@ -14,17 +18,49 @@ def interp(signal, newlen):
     return sig
 
 
-label = ub.argval('label', default='wine-bottle')
-method = ub.argval('method', default='concat')
-sampling_rate = int(ub.argval('sampling_rate', default=44100//4))
-tuned = ub.argflag('--tuned')
-show = ub.argflag('--show')
-dpath = 'test/activations'
-filenames = [os.path.join(dpath, l) for l in os.listdir(dpath) if label in l]
-if len(filenames) == 0:
-    raise ValueError('No files found of {} at {}'.format(label, dpath))
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--label',
+        default='wine_bottle',
+        help='labels to use in computation, expect to find videos in data'
+             'directory with this in its name',
+    )
+    parser.add_argument(
+        '--combo_method',
+        default='concat',
+        help='method of combining signals',
+    )
+    parser.add_argument(
+        '--sampling_rate',
+        type=int,
+        default=44100 // 4,
+        help='sampling rate for writing signal',
+    )
+    parser.add_argument(
+        '--tuned',
+        action='store_true',
+        default=False,
+        help='Whether the signals sum or attenuate each other',
+    )
+    parser.add_argument(
+        '--save_signal',
+        action='store_true',
+        default=False,
+        help='whether to save signal',
+    )
+    parser.parse_args()
+    return args
 
-raw = [utils.activations_to_audio(utils.load_activations(fn), method)
+
+args = parse_args()
+
+dpath = 'test/activations'
+filenames = [os.path.join(dpath, l) for l in os.listdir(dpath) if args.label in l]
+if len(filenames) == 0:
+    raise ValueError('No files found of {} at {}'.format(args.label, dpath))
+
+raw = [utils.activations_to_audio(utils.load_activations(fn), args.combo_method)
        for fn in filenames]
 
 # Order by decreasing length
@@ -36,21 +72,23 @@ interpped[0, :] = raw[0]
 for i in range(1, len(raw)):
     interpped[i] = interp(raw[i], interpped.shape[1])
 
-if tuned:
+if args.tuned:
     summed = interpped.prod(0)
 else:
     summed = interpped.sum(0)
 summed = 2 * (summed - summed.min()) / (summed.max() - summed.min()) - 1
 
-title = 'All {} activations {}'.format(method, label)
-if show and not os.path.isfile('graph_{}.png'.format('_'.join(title.split()))):
+# Only save signal if it doesn't exist, since it takes so long
+title = 'All {} activations {}'.format(args.combo_method, args.label)
+if args.save_signal and not os.path.isfile('graph_{}.png'.format('_'.join(title.split()))):
     print('saving graph...')
     plot.Lines(y=summed,
                title=title).save()
-audio_title = 'all-{}_{}_{}_{}-Hz'.format(
-    method,
-    'tuned' if tuned else '',
-    label,
-    sampling_rate
+
+audio_title = 'all-{}{}_{}_{:d}-Hz'.format(
+    args.combo_method,
+    '_tuned' if args.tuned else '',
+    args.label,
+    args.sampling_rate
 )
-utils.save_as_wav(summed, audio_title, sampling_rate)
+utils.save_as_wav(summed, audio_title, args.sampling_rate)
