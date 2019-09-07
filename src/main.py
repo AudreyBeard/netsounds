0673cdd47fdcd7a7c90a35c03e306a1ff4e73104
beard <joshuabeard92@gmail.com>
2019-09-07 17:01:22 -0400

    support for debugging and dpath specification, .wav saving, other minor fixes

--- src/main.py
+++ src/main.py
@@ -1,25 +1,10 @@
-import os
-
 import argparse
 import torch
 
 from models.squeezenet import TransparentSqueezeNet
 import utils
 
-# Higher verbosity means more command-line output
-VERBOSITY = 1
 
-# Download models to ~/.cache (implicitly)
-# Download data to ~/data (explicitly)
-data_dir = os.path.expandvars('$HOME/data')
-if VERBOSITY > 0:
-    print("Data location: {}".format(data_dir))
-
-#model = TransparentSqueezeNet(pretrained=True)
-#dataset = ImageNet(root=data_dir, split='val', download=True)
-
-
-# TODO test this - it's just a copy from models/test_squeezenet.py
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -31,25 +16,49 @@ def parse_args():
     parser.add_argument(
         '--image_name',
         default='wine_bottle',
-        help='image name'
+        help='Image name, or fragment of an image name. If multiple matching '
+             'files exist, will use the first (alphanumerically sorted)'
+    )
+    parser.add_argument(
+        '--dpath',
+        default=utils.IMAGES_DPATH,
+        help='location to search for images'
+    )
+    parser.add_argument(
+        '--debug',
+        action='store_true',
+        help='Drop into debugger?'
     )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    image_fpaths = utils.get_images_from_name_fragment(args.image_name)
+
+    # Initialize Imagenet dataset, image transform, and prediction model
     imagenet = utils.init_imagenet()
     transform = utils.init_image_transforms(args.image_size)
-    imgs = utils.read_images_as_tensors(image_fpaths, transform)
     model = TransparentSqueezeNet(pretrained=True)
 
-    with torch.no_grad():
-        transparent_out = model.forward_transparent(imgs)
+    # Get an image filepath that contains user-specified image name fragment
+    image_fpaths = utils.get_images_from_name_fragment(args.image_name, args.dpath)[0]
 
-    # Hardcoded based on output of forward_transparent()
-    activations = [transparent_out[i] for i in range(4)]
-    output = transparent_out[-1]
+    # Read image as a tensor for passing through the network
+    imgs = utils.read_images_as_tensors(image_fpaths, transform)
+
+    # No gradient computations need to be done, since we're not training
+    with torch.no_grad():
+
+        # Pass data through the model to get activations and output
+        transparent_out = model.forward_transparent(imgs)
+        activations = transparent_out[:-1]
+        output = transparent_out[-1]
 
     labels_pred = utils.output_to_readable(output, imagenet)
     utils.cache_activations(activations, labels_pred)
+
+    if args.debug:
+        import ipdb
+        ipdb.set_trace()
+
+    utils.save_wavs(activations, labels_pred)
