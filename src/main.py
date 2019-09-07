5562772cfbec249f90422d21fe6f3f8cb21bff9c
beard <joshuabeard92@gmail.com>
2019-09-07 10:13:28 -0400

    minor changes

--- src/main.py
+++ src/main.py
@@ -16,3 +16,37 @@ if VERBOSITY > 0:
 
 model = TransparentSqueezeNet(pretrained=True)
 #dataset = ImageNet(root=data_dir, split='val', download=True)
+
+
+# TODO test this
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--pickles',
+        action='store_true',
+        default=False,
+        help='whether to save activations as pickles'
+    )
+    parser.add_argument(
+        '--images',
+        action='store_true',
+        default=False,
+        help='whether to save activations as images'
+    )
+    parser.add_argument(
+        '--image_size',
+        type=int,
+        default=256,
+        help='image size (small edge)'
+    )
+    parser.add_argument(
+        '--image_name',
+        default='wine_bottle',
+        help='image name'
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
