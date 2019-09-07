ecb5e2be417aa25328724bbf478354a5282f2930
beard <joshuabeard92@gmail.com>
2019-09-07 18:09:14 -0400

    better argument specification, print out location of data

--- src/main.py
+++ src/main.py
@@ -29,11 +29,27 @@ def parse_args():
         action='store_true',
         help='Drop into debugger?'
     )
+    parser.add_argument(
+        '--combo_method',
+        default='concat',
+        choices=['concat', 'sum', 'none'],
+        help='Method by which we combine activation signals'
+    )
+    parser.add_argument(
+        '--sampling_rate',
+        default=441000 // 4,
+        type=int,
+        help='sampling rate for writing the file'
+    )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
+
+    if args.debug:
+        import ipdb
+        ipdb.set_trace()
 
     # Initialize Imagenet dataset, image transform, and prediction model
     imagenet = utils.init_imagenet()
@@ -55,10 +71,20 @@ if __name__ == "__main__":
         output = transparent_out[-1]
 
     labels_pred = utils.output_to_readable(output, imagenet)
-    utils.cache_activations(activations, labels_pred)
+    save_names_activations = utils.cache_activations(activations, labels_pred)
 
-    if args.debug:
-        import ipdb
-        ipdb.set_trace()
+    save_names_sounds = utils.save_wavs(
+        activations, labels_pred[0],
+        combination_method=args.combo_method,
+        sampling_rate=args.sampling_rate
+    )
 
-    utils.save_wavs(activations, labels_pred)
+    print("Activations are at:")
+    for name in save_names_activations:
+        print("  {}".format(name))
+    print("Sounds are at:")
+    if isinstance(save_names_sounds, str):
+        print("  {}".format(save_names_sounds))
+    else:
+        for name in save_names_sounds:
+            print("  {}".format(name))
